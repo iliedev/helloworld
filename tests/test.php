9407a04dea7d0f164c52599c8efe99401f187b22
Ilie Vintilescu <ilie.vintilescu@seniorinteractive.net>
2018-03-02 10:14:20 +0200

    first 2

--- tests/test.php
+++ tests/test.php
@@ -2,3 +2,10 @@
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 use HelloWorld\SayHello;
 echo SayHello::world();
+
+/*
+$ git push origin master
+The authenticity of host 'github.com (192.30.253.113)' can't be established.
+RSA key fingerprint is 16:27:ac:a5:76:28:2d:36:63:1b:56:4d:eb:df:a6:48.
+Are you sure you want to continue connecting (yes/no)? yes
+*/
